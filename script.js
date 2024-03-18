8afef70cca7a0024904a79f283cbb082b87c0dab
Rita <rk7.w2855b@gmail.com>
2024-03-18 16:01:53 -0300

    Avatar modification, light mode

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function toggleMode() {
   const html = document.documentElement
-
   //pegar a tag img
   const img = document.querySelector("#profile img")
+  const alt = document.querySelector
+
   html.classList.toggle("light")
 
   if (html.classList.contains("light")) {
-    
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
