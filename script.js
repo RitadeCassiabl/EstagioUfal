717bcd89a5b73d349f279fc451f70297a91edb20
Rita <rk7.w2855b@gmail.com>
2024-03-18 15:06:15 -0300

    tag img

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function toggleMode() {
   const html = document.documentElement
 
-  /*if (html.classList.contains("light")) {
-    html.classList.remove("light")
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  html.classList.toggle("light")
+
+  if (html.classList.contains("light")) {
+    
   } else {
-    html.classList.add("light")
-  }*/
-
-  html.classList.toggle('light')
-
-  
+    
+  }
 }
